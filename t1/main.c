52de6213fb903b293ddfb10190bfb9f2827854d3
Deivis Costa Pereira <dcpereira@inf.ufsm.br>
2019-04-20 15:43:29 -0300

    Implementa os forks

--- t1/main.c
+++ t1/main.c
@@ -1,31 +1,58 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/types.h>
-#include <sys/dir.h>
 #include <string.h>
 
-void abrirRecursivamente(const char * diretorio){
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <sys/dir.h>
+
+void dicaDia(const char * diretorio){
+    // TODO
+}
+
+void abrirRecursivamente(const char *diretorio, int primeira){
     DIR *dirp;
-    struct dirent *dp;
     dirp = opendir(diretorio);
-    char * new_dir;
-    new_dir = malloc(sizeof(char) * (strlen(diretorio) + 256));
-    while ((dp = readdir(dirp)) != NULL){
-        if (dp->d_type & DT_DIR){
-            if (dp->d_name[0] != '.'){
-                strcpy(new_dir, diretorio);
-                strcat(new_dir, dp->d_name);
-                strcat(new_dir, "/");
-                puts(new_dir);
-                abrirRecursivamente(new_dir);
+
+    if(dirp != NULL){
+        struct dirent *dfilhos;
+        char *new_dir;
+        new_dir = malloc(sizeof(char) * (strlen(diretorio) + 256));
+
+        dicaDia(diretorio);
+
+        if(!primeira)
+            printf("\t%d -> %d;\n", getppid(), getpid());
+
+        printf("\t%d [label=\"%s, %d\"];\n", getpid(), diretorio, getpid());
+
+        while ((dfilhos = readdir(dirp)) != NULL){
+            if (dfilhos->d_type & DT_DIR){
+                if (dfilhos->d_name[0] != '.'){
+                    strcpy(new_dir, diretorio);
+                    strcat(new_dir, dfilhos->d_name);
+                    strcat(new_dir, "/");
+
+                    int pid = fork();
+                    if (pid == 0){
+                        abrirRecursivamente(new_dir, 0);
+                        return; 
+                    }
+                }
             }
+            wait(NULL); 
+        }
+        closedir(dirp);
+
+        if(primeira){
+            puts("}");    
         }
     }
-    closedir(dirp);
 }
 
 int main(int argc, char const *argv[]){
-    
-    abrirRecursivamente("/home/cpdeivis/playground/");
+    puts("digraph G {");
+    abrirRecursivamente("./home/estagiario/", 1);
     return 0;
 }
